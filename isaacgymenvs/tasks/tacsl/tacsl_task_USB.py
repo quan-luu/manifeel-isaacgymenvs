642173a75f487ce7b13b933c8262c4eef22930ef
quan-luu <luuquan.bku@gmail.com>
2025-08-02 18:27:40 -0400

    update assests for usb/power plug insertion task

--- isaacgymenvs/tasks/tacsl/tacsl_task_USB.py
+++ isaacgymenvs/tasks/tacsl/tacsl_task_USB.py
@@ -614,22 +614,11 @@ class TacSLTaskUSB(TacSLTaskImageAugmentation, TacSLEnvInsertion, FactoryABCTask
                                          tip_dist_range_mag / 2. *
                                          self.cfg_task.randomize.plug_pos_z_in_gripper_noise_multiplier)
         # subtract from plug length to get the distance from the tip
-        fine_tuning_z = 0.0 #-0.05  # 👆-0.05 #Positive values means downward
-        ee_to_plug_tip_pos_local[:, 2] = plug_pos_in_gripper_z_sampled - self.plug_lengths.squeeze(-1) + fine_tuning_z
-
-        #plug_pos_in_gripper_noise[:, :2] = torch.tensor([[1.0, 2.0]], device="cuda:0")
-        #self.cfg_task.randomize.plug_pos_in_gripper_noise_xy = [-0.5, 0.2]  # 
+        ee_to_plug_tip_pos_local[:, 2] = plug_pos_in_gripper_z_sampled - self.plug_lengths.squeeze(-1)
 
         plug_pos_in_gripper_xy_sampled = plug_pos_in_gripper_noise[:, :2] @ torch.diag(
             torch.tensor(self.cfg_task.randomize.plug_pos_in_gripper_noise_xy, device=self.device))
-        #print("plug_pos_in_gripper_xy_sampled:", plug_pos_in_gripper_xy_sampled)
-        #print("plug_pos_in_gripper_noise_xy:", self.cfg_task.randomize.plug_pos_in_gripper_noise_xy)
-
-
         ee_to_plug_tip_pos_local[:, :2] = plug_pos_in_gripper_xy_sampled
-        print("👀ee_to_plug_tip_pos_local:", ee_to_plug_tip_pos_local.cpu().detach().numpy())
-        # print(f' plug_pos_in_gripper_xy_sampled: {plug_pos_in_gripper_xy_sampled}')
-        # print(f' plug_pos_in_gripper_noise[:, :2]: {plug_pos_in_gripper_noise[:, :2]}')
 
         world_to_plug_tip_quat, world_to_plug_tip_pos = torch_jit_utils.tf_combine(self.fingertip_midpoint_quat,
                                                                                    self.fingertip_midpoint_pos,
@@ -641,9 +630,6 @@ class TacSLTaskUSB(TacSLTaskImageAugmentation, TacSLEnvInsertion, FactoryABCTask
             2 * (torch.rand((self.num_envs, 3), dtype=torch.float32, device=self.device) - 0.5)  # [-1, 1]
         plug_noise_rot_in_gripper *= torch.tensor(self.cfg_task.randomize.plug_noise_rot_in_gripper,
                                                   device=self.device).expand(self.num_envs, 3)
-        
-        # print(f' plug_noise_rot_in_gripper: {plug_noise_rot_in_gripper}')
-        
         # z is along the axis of the gripper so it has no effects for round pegs
         # y rotates in and out of the gripper axis
         # x rot should be zero
@@ -710,7 +696,7 @@ class TacSLTaskUSB(TacSLTaskImageAugmentation, TacSLEnvInsertion, FactoryABCTask
                                                      gymtorch.unwrap_tensor(self.root_state),
                                                      gymtorch.unwrap_tensor(plug_socket_actor_ids_sim),
                                                      len(plug_socket_actor_ids_sim))
-
+        
     def _reset_buffers(self, env_ids):
         """Reset buffers. """
 
@@ -854,27 +840,14 @@ class TacSLTaskUSB(TacSLTaskImageAugmentation, TacSLEnvInsertion, FactoryABCTask
     def _check_plug_close_to_socket(self):
         """Check if plug is close to socket."""
 
-        # TO-DO: update following threshold values in the config file
-        USB_lower = 0.30
-        USB_upper = 0.35
-        lower = USB_lower
-        upper = USB_upper
+        keypoint_dist = torch.norm(self.keypoints_socket - self.keypoints_plug, p=2, dim=-1)
 
-        keypoint_dist = torch.norm(self.keypoints_socket - self.keypoints_plug, 
-                                   p=2, 
-                                   dim=-1)
-        
-        key_point_dis_mean = torch.mean(keypoint_dist, dim=-1)
-        
-        is_plug_close_to_socket = torch.where(
-            (self.cfg_task.rl.close_error_thresh_lower < key_point_dis_mean) & 
-            (key_point_dis_mean < self.cfg_task.rl.close_error_thresh_upper),
-            torch.ones_like(self.progress_buf),
-            torch.zeros_like(self.progress_buf)
-        )       
-                
+        is_plug_close_to_socket = torch.where(torch.mean(keypoint_dist, dim=-1) < self.cfg_task.rl.close_error_thresh,
+                                              torch.ones_like(self.progress_buf),
+                                              torch.zeros_like(self.progress_buf))
+
         return is_plug_close_to_socket
-
+    
     def _check_plug_is_centered_on_socket(self):
         """Get distance of the tip of the peg below the level of the socket, when the peg is not centered.
         Used to penalize a common failure case when the peg is upright beside the socket and not inside the socket
